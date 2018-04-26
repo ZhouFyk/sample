720caf1cf47a73ad42d4924642461284970e0688
fuyukang <fuyukang.me@foxmail.com>
2018-04-26 03:47:45 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-	<h1>主页</h1>
+	<div class="jumbotron">
+		<h1>Hellp Laravel</h1>
+		<p class="lead">
+			你现在看到的是 <a href="#">SB</a>
+		</p>
+		<p>
+			come on
+		</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+		</p>
+	</div>
 @stop
